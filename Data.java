27b74ea6db43049fa8e3df8229c1ad607a1e46ce
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2024-10-09 10:16:05 -0400

    data done

---
+++ Data.java
@@ -0,0 +1,22 @@
+
+/*This class represents the records that will be stored in the HashDictionary.  */
+public class Data {
+    private String config;
+    private int score;
+
+    /*A constructor which initializes a new Data object with the specified configuration and score. */
+    public Data(String config, int score){
+            this.config = config;
+            this.score = score;
+    }   
+    
+    /*Returns the configuration stored in this Data object. */
+    public String getConfiguration(){
+        return config;
+    }
+
+    /*Returns the score in this Data. */
+    public int getScore(){
+        return score;
+    }
+}
